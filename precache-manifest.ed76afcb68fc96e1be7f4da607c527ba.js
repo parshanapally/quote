756958512e54d66b4964875243479271222fdac7
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-04-15 09:20:23 -0500

    Updates

---
+++ precache-manifest.ed76afcb68fc96e1be7f4da607c527ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ea8c16f055c17cefe30",
+    "url": "/quote/static/js/runtime~main.7f5c834d.js"
+  },
+  {
+    "revision": "73faa0eeee4ff99052be",
+    "url": "/quote/static/js/main.38b92227.chunk.js"
+  },
+  {
+    "revision": "2c13528a6770948553cb",
+    "url": "/quote/static/js/2.fd367ab0.chunk.js"
+  },
+  {
+    "revision": "73faa0eeee4ff99052be",
+    "url": "/quote/static/css/main.56ed918d.chunk.css"
+  },
+  {
+    "revision": "3a938b9baa37c5f79599c57b31da0641",
+    "url": "/quote/index.html"
+  }
+];
